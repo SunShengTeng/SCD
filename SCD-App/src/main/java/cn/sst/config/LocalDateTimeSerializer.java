713c4be9f74f276af6ce9ff92a5cff6a2153888a
sunshengteng <sunshengteng@yahoo.com>
2020-09-15 13:58:04 +0800

    新增：乱七八糟一些东西

---
+++ SCD-App/src/main/java/cn/sst/config/LocalDateTimeSerializer.java
@@ -0,0 +1,22 @@
+package cn.sst.config;
+
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.JsonSerializer;
+import com.fasterxml.jackson.databind.SerializerProvider;
+
+import java.io.IOException;
+
+/**
+ * @author shengtengsun
+ * @Description
+ * @Date 2020/9/8 11:32 上午
+ * @Version 1.1.0
+ **/
+public class LocalDateTimeSerializer extends JsonSerializer {
+    @Override
+    public void serialize(Object value, JsonGenerator gen, SerializerProvider serializers) throws IOException {
+
+    }
+
+
+}
